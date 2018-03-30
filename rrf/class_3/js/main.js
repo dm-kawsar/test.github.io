69daf9c8101ad0f45c1eb26176f02292f69ffc26
dm-kawsar <37432986+dm-kawsar@users.noreply.github.com>
2018-03-30 10:43:01 +0600

    rrf/class-3

---
+++ rrf/class_3/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	$('.video-play-btn').magnificPopup({
+    		type: 'video', 
+    	});
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
