eca6a4d25ae030608cf90063b05af0ef042f695f
dm-kawsar <37432986+dm-kawsar@users.noreply.github.com>
2018-04-02 22:32:27 +0600

    problem/class-3

---
+++ problem/class-3/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+
+
+    jQuery(document).ready(function($){
+
+    	$('.video-play-btn').magnificPopup({
+    		type: 'iframe', 
+    	});
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
