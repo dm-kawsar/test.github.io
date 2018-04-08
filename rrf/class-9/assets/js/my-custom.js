0a6559ff969f2b84ada993c62df87ab05d4109f5
dm-kawsar <37432986+dm-kawsar@users.noreply.github.com>
2018-04-08 17:30:28 +0600

    rrf/class-9

---
+++ rrf/class-9/assets/js/my-custom.js
@@ -0,0 +1,12 @@
+(function($){
+	jQuery(document).ready(function($){
+		// owl-carousel
+		$(".client-carousel-box").owlCarousel({
+			items: 3,
+			loop: true,
+			autoplay: false,
+			margin: 20,
+		});
+		
+	});
+})(jQuery)
