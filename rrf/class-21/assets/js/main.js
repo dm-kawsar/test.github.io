929eeb02c0d250d03906cbbcc3118e0e8b811019
dm-kawsar <37432986+dm-kawsar@users.noreply.github.com>
2018-04-26 15:48:14 +0600

    rrf/class-21

---
+++ rrf/class-21/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $('.hompage-slider-wrap').owlCarousel({
+        	items: 1,
+        	loop: true,
+        	nav: false,
+        	dots: true,
+        	autoplay: false,
+        });
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
