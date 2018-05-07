145e619fd24ce3d51d5018c438207c36f3657ac4
dm-kawsar <37432986+dm-kawsar@users.noreply.github.com>
2018-05-07 18:30:05 +0600

    rrf/class-31

---
+++ rrf/class-31/assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+			var s = skrollr.init({
+				render: function (data) {
+
+				}
+			});
+
+    });
+
+
+    jQuery(window).load(function(){
+
+
+
+    });
+
+
+}(jQuery));
