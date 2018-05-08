5b450f4eb70526bcfc67565b369d7d6940552b1f
dm-kawsar <37432986+dm-kawsar@users.noreply.github.com>
2018-05-08 08:34:54 +0600

    rrf/class-32

---
+++ rrf/class-32/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+
+    $(document).ready(function($){
+
+			$(window).scroll(function(){
+				if ($(this).scrollTop() > 200) {
+					$('.scroll-btn').fadeIn();
+				} else {
+					$('.scroll-btn').fadeOut();
+				}
+			});
+
+    });
+
+		$('.scroll-btn').on('click', function (e) {
+			e.preventDefault();
+			$('html, body').animate({scrollTop: 0}, 800);
+		});
+
+
+
+
+
+}(jQuery));
