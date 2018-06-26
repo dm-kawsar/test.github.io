124f2936f06dedf895965c84fc619850deeeaa0a
Kawsar Mahmud <mee-kawsar@users.noreply.github.com>
2018-06-26 22:07:47 +0600

    rrf/class-46

---
+++ rrf/class-46/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+			$('.featured-trailor-carousel').owlCarousel({
+				items: 1,
+				loop: true,
+				autoplay: false,
+				nav: false,
+				dots: true,
+			});
+
+    });
+
+
+    jQuery(window).load(function(){
+
+
+
+    });
+
+
+}(jQuery));
